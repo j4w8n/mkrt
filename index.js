af61dd39f599370734f6900e6da3bf7e28e0adf6
Jason Creviston <jcreviston@pm.me>
2022-08-05 08:31:08 -0400

    checks route file existence, not empty directory

--- index.js
+++ index.js
@@ -21,29 +21,37 @@ program
 
 program
   .name('mkrt')
-  .usage('<directory-name>')
-  .argument('<directory-name>', 'route to create')
-  .description('Easily create SvelteKit route directories and files.')
+  .usage('<path>')
+  .argument('<path>', 'route to create')
+  .description('Create SvelteKit routes, fast')
   .version(packageJson.version)
-  .option('-n, --named-layout <name>', 'use this named layout for the route')
+  .option('-n, --named-layout <layout-name>', 'use this named layout for the route')
   .option('-c, --codekit', 'add sensible starter code into files')
   .option('--no-codekit', 'do not use codekits in files')
   .option('-t, --typescript', 'use .ts files')
   .addOption(new Option('-j, --javascript', 'use .js files').conflicts('typescript'))
   .addOption(new Option('-p, --page', 'route is a +page.svelte file'))
-  .addOption(new Option('-s, --server', 'route is a +server.[ts|js] file').conflicts('page'))
+  .addOption(new Option('-s, --server', 'route is a +server.[ts|js] file').conflicts('page').conflicts('namedLayout'))
   .action(async (name, options) => {
     let files
-    const dir = 'src/routes'
-    const full_path = name === '.' ? dir : path.join(dir, name)
+    const root = 'src/routes'
+    const dir_path = name === '.' ? root : path.join(root, name)
     const named_layout = options.namedLayout ? `@${options.namedLayout}` : ''
     const language = options.typescript ? 'ts' : options.javascript ? 'js' : config.language
     const route = options.server ? 'server' : options.page ? 'page' : config.route
     const codekit = options.codekit ?? config.codekit == "true"
 
+    const create_file = (src, dest) => {
+      try {
+        fs.copyFileSync(src, dest)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     try {
-      if (!fs.existsSync(dir)) {
-        console.log(`Cannot find ${dir}. Are you in the correct directory?`)
+      if (!fs.existsSync(root)) {
+        console.log(`Cannot find ${root}. Are you in your project's root directory?`)
         process.exit(1)
       }
     } catch (error) {
@@ -59,57 +67,50 @@ program
         break;
     } 
 
-    console.log('Checking path...')
-    try {
-      if (fs.existsSync(full_path)) {
-        if (fs.readdirSync(full_path).length > 0) {
-          const response = await prompts({
-            type: 'confirm',
-            name: 'value',
-            message: 'Directory not empty. Continue?',
-            initial: false
-          })
-    
-          if (!response.value) {
-            process.exit(1);
-          }
-        }
-      } else {
-        fs.mkdirSync(full_path, { recursive: true })
+    any_exist = files.some(file => fs.existsSync(path.join(dir_path, file)))
+    if (any_exist) {
+      let response = await prompts({
+        type: 'confirm',
+        name: 'value',
+        message: `One or more route files exist in ${dir_path}! Overwrite?`,
+        initial: false
+      })
+
+      if (!response.value) {
+        process.exit(1);
       }
-    } catch (error) {
-      console.log(error)
     }
+
+    fs.mkdirSync(dir_path, { recursive: true })
 
     if (!codekit) {
       // create files with no content
       files.forEach(file => {
         try {
-          fs.writeFileSync(`${full_path}/${file}`,'')
+          fs.writeFileSync(path.join(dir_path, file),'')
         } catch (error) {
           console.log(error)
         }
       })
     } else {
-      files.forEach(dest => {
-        let src = dest
-        const parts = dest.split('@')
+      for (let i = 0; i < files.length; i++) {
+        let template = files[i]
+        const parts = files[i].split('@')
         if (parts.length > 1 ) {
-          // filename has a named layout, adjusting src for filename copy
-          const ext = parts[1].split('.')       
-          src = `${parts[0]}.${ext.slice(1).join('.')}`
+          // file has a named layout, adjusting template filename for copy
+          const suffix = parts[1].split('.')       
+          template = `${parts[0]}.${suffix.slice(1).join('.')}`
         }
-        if ((dest.split('.')[1] === 'svelte') && (!dest.split('.')[2])) {
-          // use .js or .ts +page.svelte file, for src filename copy
-          src = `${src}.${language}`
+        if (files[i].split('.')[1] === 'svelte') {
+          // add .js or .ts to end of +page.svelte file, for template filename copy
+          template = `${template}.${language}`
         }
 
-        try {
-          fs.copyFileSync(path.join(__dirname, 'templates', src), path.join(full_path, dest))
-        } catch (error) {
-          console.log(error)
-        }
-      })
+        const src = path.join(__dirname, 'templates', template)
+        const dst = path.join(dir_path, files[i])
+
+        create_file(src, dst)
+      }
     }
     console.log('Route created!')
   })
